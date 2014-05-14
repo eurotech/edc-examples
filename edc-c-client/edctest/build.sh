94ebf030c0ceb5695a9e643aa3bc66d1d31436f4
Pierre <pierre.pitiot@eurotech.com>
2014-05-14 15:41:12 +0200

    Modify build script to run on recent linux systems.

--- edc-c-client/edctest/build.sh
+++ edc-c-client/edctest/build.sh
@@ -2,9 +2,9 @@
 
 . ./mqttlib_dir.sh
 
-gcc -Wall \
+g++ -Wall \
  -I../edcclient/ -I../protobuf/src/ -I../org.eclipse.paho.mqtt.c/src/ -I../config \
  -L../protobuf/src/.libs/ -L../org.eclipse.paho.mqtt.c/build/${MQTTLIB_DIR}/ \
  -o edctest \
  edctest.cpp ../edcclient/edcpayload.pb.cc ../edcclient/config.cpp \
- -lmqttv3c -lprotobuf
+ -lmqttv3c -lpthread -lprotobuf
